c15db23a8c225683158f6945c302061abf22ea29
AK Maurya <ankitkrmaurya82@gmail.com>
2025-02-22 22:04:01 +0530

    add fronted

---
+++ fronted/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import { ThemeProvider } from './contexts/theme'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>,
+)
