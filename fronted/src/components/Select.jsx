86ee61b5ffe37c2d03be85d386e1a60d0b2df29b
AK Maurya <ankitkrmaurya82@gmail.com>
2025-03-14 17:06:12 +0530

    add pages like postcard, select, login

---
+++ fronted/src/components/Select.jsx
@@ -0,0 +1,28 @@
+import React, {useId} from 'react'
+
+function Select({
+    options,
+    label,
+    className='',
+    ...props
+}, ref) {
+    const id = useId()
+    return (
+        <div className='w-full'>
+            {label && <label htmlFor={id} className=''></label>}
+            <select 
+            {...props}
+            id={id}
+            className={``}
+            >
+                {options?.map((option) => (
+                    <option key={option} value={option}>
+                        
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default React.forwardRef(Select)
